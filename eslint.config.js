d6fca09d247442932001c621fe5eaa8850826a36
Alexandr Bokov <pd.sk@bk.ru>
2026-04-08 15:11:52 +0300

    add  test jest

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,26 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import jest from 'eslint-plugin-jest'
 
-export default defineConfig([
-  globalIgnores(["dist"]),
+export default [
+  js.configs.recommended,
+
   {
-    files: ["**/*.{js,jsx}"],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
+      globals: {
+        ...globals.browser,
       },
     },
-    rules: {
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+  },
+
+  {
+    files: ['**/*.test.{js,jsx}', '**/__tests__/**/*.{js,jsx}'],
+    ...jest.configs['flat/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
     },
   },
-]);
+]
---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  rootDir: '.',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  testMatch: ['<rootDir>/src/**/*.test.js'],
+  moduleFileExtensions: ['js', 'jsx'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/fileMock.cjs',
+  },
+}
